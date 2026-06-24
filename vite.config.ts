287ebc1c63717f3b999cb3ca2698d6fcbf5261d7
Otinelio <otidumeando@gmail.com>
2026-06-24 01:21:57 +0000

    r

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite"; // <-- Ajout de l'import Nitro
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   vite: { // <-- Ajout de la clé vite pour passer des plugins supplémentaires
     plugins: [
-      nitro({ preset: "vercel" }) // <-- Force le formatage pour Vercel
+      nitro({ preset: "vercel" }), // <-- Force le formatage pour Vercel
+      sitemap({
+        hostname: 'https://residencetogoliving.com',
+        dynamicRoutes: [
+          '/',
+          '/a-propos',
+          '/carrieres',
+          '/contact',
+          '/evenements',
+          '/galerie',
+          '/hebergements',
+          '/loisirs',
+          '/reserver',
+          '/restaurant'
+        ],
+      })
     ]
   },
   tanstackStart: {
